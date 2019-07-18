72d882cda18cc908a0f80c96b8a694237ec99c55
Steven <stevenlin1015@gmail.com>
2019-07-18 19:30:29 +0800

    Initial Commit

---
+++ myTableView1/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  myTableView1
+//
+//  Created by Steven on 2019/7/17.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+    
+    let items = ["item 1", "item 2", "item 3"]
+
+    let cellId = "cellId"
+    let headerId = "headerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "My TableView"
+        tableView.register(MyCell.self, forCellReuseIdentifier: cellId)
+        tableView.register(Header.self, forHeaderFooterViewReuseIdentifier: headerId)
+        
+        tableView.sectionHeaderHeight = 50
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! MyCell
+        myCell.nameLabel.text = items[indexPath.row]
+        return myCell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterView(withIdentifier: headerId)
+    }
+    
+   
+}
+
+class Header: UITableViewHeaderFooterView {
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "My Header"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: .init(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: .init(), metrics: nil, views: ["v0": nameLabel]))
+    }
+}
+
+class MyCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sample Item"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Delete", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addSubview(actionButton)
+        
+        actionButton.addTarget(self, action: #selector(handleAction), for: .touchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-8-[v1(80)]-8-|", options: .init(), metrics: nil, views: ["v0": nameLabel, "v1": actionButton]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: .init(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: .init(), metrics: nil, views: ["v0": actionButton]))
+    }
+    
+    @objc func handleAction() {
+        print("tapped")
+    }
+}
